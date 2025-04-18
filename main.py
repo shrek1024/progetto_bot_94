803b66c2b9b04a4b5e62e0d4795cc15ecbb2a698
Marco Panseri <marpanseri@gmail.com>
2025-04-18 18:46:51 +0200

    Legal header added/ basic imports

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-x  = 0
-print(x)
-#ciao
+#/*--
+#
+# Copyright (c) 2025  Frêney Studios
+#
+# Module Name:
+#
+#	 main.py
+#
+# Abstract:
+#
+#	 Safe Aero Core
+#
+# Author:
+#
+#	 Marco Panseri (Marx) 18-04-2025
+#    Piergiorgio (Shrek1024) 18-04-2025
+#    Pradee 18-04-2025
+#    Tommaso 18-04-2025
+#
+# Revision History:
+#
+#--*/
+
+import tensorflow as tf
+import numpy as np
+import requests 
+import keras
