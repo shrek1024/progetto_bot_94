79fab762c514649befb0d6a9ff76bda11b2bcd8c
Marco Panseri <marpanseri@gmail.com>
2025-04-18 19:01:00 +0200

    V.0.0.1

--- main.py
+++ main.py
@@ -13,18 +13,39 @@
 # Author:
 #
 #	 Marco Panseri (Marx) 18-04-2025
-#    Piergiorgio (Shrek1024) 18-04-2025
-#    Pradee 18-04-2025
-#    Tommaso 18-04-2025
+#  Piergiorgio (Shrek1024) 18-04-2025
+#  Pradee 18-04-2025
+#  Tommaso 18-04-2025
 #
 # Revision History:
 #
 #--*/
 
-import tensorflow as tf
-import numpy as np
-import requests 
-import keras
-import discord
-import yolov
-import imageai
+################### IMPORTS #############################
+try:
+	import tensorflow as tf
+	import numpy as np
+	import requests 
+	import keras
+	import discord
+	import yolov
+	import imageai
+except Exception as e:
+	print("Error: " + e.message)
+
+################### CFG VARIABLES #######################
+show_module_ver : bool = True
+version 			  : str  = "v.0.0.1"
+legalcaption 	  : str  = "Copyright (c) Frêney Studios and affiliates"
+
+################### MODULE VERSIONS #######################
+if show_module_ver:
+	print("Module information")
+	print(" - Tensorflow: " + tf.__version__)
+	print(" - NumPy:		  " + np.__version__)
+	print(" - requests: 	" + requests.__version__)
+	print(" - keras: 			" + keras.__version__)
+	print(" - discord:		" + discord.__version__)
+	print(" - YOLOv:			" + yolov.__version__)
+	print(" - ImageAI:		" + imageai.__version__)
+
